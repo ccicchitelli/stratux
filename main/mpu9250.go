5a47a445582fea380e26256babf343e88d035d2d
ccicchitelli <c.cicchitelli@gmail.com>
2016-07-13 21:48:20 -0400

    First test of new zeta compensation

--- main/mpu9250.go
+++ main/mpu9250.go
@@ -164,7 +164,7 @@ func readRawData() {
 		y_mag_f := float64(int16(x_mag)) * 1.28785103785104 * magYcal
 		z_mag_f := float64(int16(-z_mag)) * 1.28785103785104 * magZcal
 
-		AHRSupdate(convertToRadians(x_gyro_f), convertToRadians(y_gyro_f), convertToRadians(z_gyro_f), float64(x_acc_f), float64(y_acc_f), float64(z_acc_f), float64(x_mag_f), float64(y_mag_f), float64(z_mag_f))
+		AHRSupdateNew(convertToRadians(x_gyro_f), convertToRadians(y_gyro_f), convertToRadians(z_gyro_f), float64(x_acc_f), float64(y_acc_f), float64(z_acc_f), float64(x_mag_f), float64(y_mag_f), float64(z_mag_f))
 	}
 }
 
